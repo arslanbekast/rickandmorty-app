ff94fafe17e4f9f505b1bd6e4e94baf1bf8d58a7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-02-09 16:25:49 +0300

    feat: add charactersApi

---
+++ src/features/characters/api/charactersApi.ts
@@ -0,0 +1,15 @@
+import { rickAndMortyApi } from '@/common/api/commonApi'
+import { Character, RickAndMortyApiResponse } from '@/features/characters/api/charactersApi.types'
+import { fetchCharactersParams } from '@/features/characters/model/charctersSlice'
+
+export const charactersApi = {
+  getCharacters(params: fetchCharactersParams) {
+    return rickAndMortyApi.get<RickAndMortyApiResponse>('character', { params })
+  },
+  getMultipleCharacters(ids: number[]) {
+    return rickAndMortyApi.get<Character[]>(`character/${ids}`)
+  },
+  getCharacterById(id: number) {
+    return rickAndMortyApi.get<Character>(`character/${id}`)
+  },
+}
