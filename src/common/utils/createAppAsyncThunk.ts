cef5a3a1e520bf0117c201b580c8bb76e51435d7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-02-09 16:28:26 +0300

    feat: create createAppAsyncThunk

---
+++ src/common/utils/createAppAsyncThunk.ts
@@ -0,0 +1,11 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { AppDispatch, RootState } from '@/app/store'
+
+/**
+ Эта функция предназначена для того, чтобы избавиться от дублирования кода по созданию типов в санке
+ */
+export const createAppAsyncThunk = createAsyncThunk.withTypes<{
+  state: RootState
+  dispatch: AppDispatch
+  rejectValue: null | string
+}>()
