cd1221fd201c7c3a20bb044c7224d79f970530ec
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-02-09 16:41:44 +0300

    chore: install gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import * as path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'rickandmorty-app',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
